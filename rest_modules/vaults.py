1b9fc1b42ff310cce92b533fec594916b843314b
Korostylev Alexandr <korostylev-aa@rr.paymgate.ru>
2024-06-03 12:44:28 +0300

    rest_modules use http_client

--- rest_modules/vaults.py
+++ rest_modules/vaults.py
@@ -1,10 +1,9 @@
-import requests
-
-
 from rest_modules import is_failed_status_code
+import utils.http_client as http_client
 
 
 def get_vault(vault_id: str, options):
+    requests: http_client.HttpClientProtocol = options.http_session
     # receive vault item
     response = requests.get(
         url=f"{options.host}/vaults/{vault_id}",
